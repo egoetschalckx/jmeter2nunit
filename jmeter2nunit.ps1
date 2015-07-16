3ec96e0aa41534f79632bbd76abd7103da1e50de
Eric Goetschalckx <drax1983@gmail.com>
2015-07-15 21:39:39 -0400

    rename files

--- jmeter2nunit.ps1
+++ jmeter2nunit.ps1
@@ -1,3 +1,11 @@
+param ($xml, $xsl, $output, $reportname)
+
+if (-not $xml -or -not $xsl -or -not $output -or -not $reportname)
+{
+	Write-Host "& .\xslt.ps1 [-xml] xml-input [-xsl] xsl-input [-output] transform-output [$reportname] report-name"
+	exit;
+}
+
 trap [Exception]
 {
 	Write-Host $_.Exception;
@@ -5,9 +13,9 @@ trap [Exception]
 }
 
 $xsl = "F:/Projects/jmeter2nunit/jmeter_to_nunit.xslt"
-$xml = "F:/Projects\jmeter2nunit/test/mpplat_jmeter_report.xml"
+$xml = "F:/Projects\jmeter2nunit/test/mpts_jmeter_report.xml"
 $reportName = "Platform PreProd Automated Tests"
-$output = "F:/Projects/jmeter2nunit/nunit_report.xml"
+$output = "F:/Projects/jmeter2nunit/nunit_report2.xml"
 
 $xslt = New-Object System.Xml.Xsl.XslCompiledTransform;
 $xslt.Load($xsl);
